7d3dc49f6fcebe6b97257b4e7f2fd51f3ebacd27
Tim <tim.r.burr@gmail.com>
2023-12-10 15:20:56 -0700

    day 7 progress

--- 2023/day07/part1.py
+++ 2023/day07/part1.py
@@ -1,6 +1,18 @@
 
 CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
-with open('input.txt') as fp:
-    for line in fp:
-        parse(line.strip())
+def sort(hand_str):
+    return ""
+
+with open('input.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+hands = []
+for line in lines:
+    data = line.split(" ")
+    hand = sort(data[0])
+    bid = int(data[1])
+    hands.append((hand, bid))
+
+for hand in hands:
+    print(hand)

8685432710b15ab24c46a8c1eda20436580b9b37
Tim <tim.r.burr@gmail.com>
2023-12-11 15:19:58 -0700

    day 9

---
+++ 2023/day09/part2.py
@@ -0,0 +1,22 @@
+
+def extrapolate_value(line: str) -> int:
+    seqs = []
+    steps = [int(n) for n in line.split()]
+    while len(steps) > 0 and set(steps) != {0}:
+        seqs.append(steps)
+        steps = [steps[i + 1] - steps[i] for i in range(len(steps) - 1)]
+    
+    sum = 0
+    for seq in seqs[::-1]:
+        sum = seq[0] - sum
+    return sum
+    
+
+sum = 0
+
+with open('input.txt') as file:
+    for line in file:
+        #print(line)
+        sum += extrapolate_value(line.strip())
+        
+print(str(sum))

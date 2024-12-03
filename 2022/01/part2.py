5f8594e4540cc34165198f203f73fd2b51502c42
Tim <Tim.Burr@netacent.com>
2024-12-03 14:09:03 -0700

    day 1, part 2 (authenticated w/ GitHUb)

--- 2022/01/part2.py
+++ 2022/01/part2.py
@@ -1,18 +1,27 @@
 
-top_three = []
+top_three = [-1, -1, -1]
 
-with open('test.txt') as fp:
-    
+def try_insert(num):
+    if num > top_three[0]:
+        top_three[2] = top_three[1]
+        top_three[1] = top_three[0]
+        top_three[0] = num
+    elif num > top_three[1]:
+        top_three[2] = top_three[1]
+        top_three[1] = num
+    elif num > top_three[2]:
+        top_three[2] = num
+
+with open('input.txt') as fp:
+    sum = 0
     for line in fp:
         if line == '\n':
-            if sum > max:
-                max = sum
+            try_insert(sum)
             sum = 0
         else:
             sum += int(line)
 
     # check one more time:
-    if sum > max:
-        max = sum
+    try_insert(sum)
 
-print(max)
+print(str(top_three[0] + top_three[1] + top_three[2]))

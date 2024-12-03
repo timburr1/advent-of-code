458c848211317ed51843a16088802ecef5540058
Tim Burr <tim.r.burr@gmail.com>
2024-12-02 18:24:38 -0700

    day 1, part 1

--- 2024/02/part1.py
+++ 2024/02/part1.py
@@ -3,30 +3,44 @@ safe_count = 0
 
 def all_increasing(readings):
     for i in range(1, len(readings)):
-        if readings[i] <= readings[i-1]:
-            return false
+        if int(readings[i]) <= int(readings[i-1]):
+            #print('Not increasing: ' + str(readings))
+            return False
 
-    return true
+    return True
 
 def all_decreasing(readings):
     for i in range(1, len(readings)):
-        if readings[i] >= readings[i-1]:
-            return false
+        if int(readings[i]) >= int(readings[i-1]):
+            #print('Not decreasing: ' + str(readings[i-1]) + ', ' + str(readings[i]))
+            return False
 
-    return true
+    return True
+
+def adjacent_diffs_valid(readings):
+    for i in range(1, len(readings)):
+        if abs(int(readings[i]) - int(readings[i-1])) > 3:
+            return False
+
+    return True
 
 def check(str):
+    global safe_count
+
     readings = str.replace('\n', '').split(' ')
 
-    if not all_increasing(readings) and not all_decreasing(readings):
+    if not (all_increasing(readings) or all_decreasing(readings)):
+        #print('Not increasing/decreasing: ' + str)
         return  
     
-    if too_far_apart(readings):
+    if not adjacent_diffs_valid(readings):
+        #print('Invalid distance: ' + str)
         return
 
+    #print('Safe: ' + str)
     safe_count += 1
 
-with open('test.txt') as fp:
+with open('input.txt') as fp:
     for line in fp:
         check(line)
 

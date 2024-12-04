115a4a2e6a4a1590b0b52d466a0123e8d1e8333f
Tim <Tim.Burr@netacent.com>
2024-12-04 10:18:54 -0700

    part 2

--- 2021/01/part2.py
+++ 2021/01/part2.py
@@ -5,10 +5,14 @@ with open('2021/01/input.txt') as fp:
     for line in fp:
         nums.append(int(line.replace('\n', '')))
 
+sums = []
+for i in range(2, len(nums)):
+    sums.append(nums[i] + nums[i-1] + nums[i-2])
+
 count = 0
 
-for i in range(1, len(nums)):
-    if nums[i] > nums[i-1]:
+for i in range(1, len(sums)):
+    if sums[i] > sums[i-1]:
         count += 1
 
 print(str(count))

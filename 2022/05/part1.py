115f6d320188577c3905edbfc6eea1be430f9872
Tim Burr <tim.r.burr@gmail.com>
2024-12-04 21:19:22 -0700

    small update

--- 2022/05/part1.py
+++ 2022/05/part1.py
@@ -1,4 +1,8 @@
 
+# todo: create stacks
+# move crates according to input
+# pop each stack + print
+
 with open('2022/05/test.txt') as fp:    
     for line in fp:
         print(line.replace('\n', ''))

66b2c5c1668a2a3540a2937f45f4278ee84f4a6b
Tim <Tim.Burr@netacent.com>
2024-12-10 14:48:12 -0700

    day 9, part 2 (93*)

--- 2024/09/part2.py
+++ 2024/09/part2.py
@@ -1,58 +1,57 @@
 import sys
 
 def expand(dm):
-    result = []
-    c = 0
-    for i in range(0, len(dm), 2):
-        file_len = int(dm[i])
-        if i < len(dm) - 1:
-            empty_len = int(dm[i+1])
-        else:
-            empty_len = 0
-
-        for j in range(0, file_len):
-            result.append(str(c))
-        for k in range(0, empty_len):
-            result.append('.')
-        
-        c += 1
-
-    return result
-
-def compress(dm):    
-    i = 0
-    j = len(dm)-1
-    while i < j:
-        if dm[i] != '.':
-            i += 1 
-            continue
-        while dm[j] == '.':
-            j -= 1
-        
-        dm = dm[:i] + [dm[j]] + dm[i+1:j] + ['.'] + dm[j+1:]
-        
-    return dm
-
-def checksum(dm):
-    sum = 0
+    free_blocks = []
+    file_blocks = []
 
     for i in range(0, len(dm)):
-        if dm[i] == '.':
+        if i % 2 == 0:
+            file_blocks.append(int(dm[i]))
+        else:
+            free_blocks.append(int(dm[i]))
+
+    blocks = []
+    for i in range(len(file_blocks)):
+        blocks.append([str(i)] * file_blocks[i])
+        if i < len(free_blocks):
+            blocks.append(['.'] * free_blocks[i])
+    return blocks
+
+def compress(blocks):    
+    for file_index in range(len(blocks)-1, -1, -1):
+        file_block = blocks[file_index]
+
+        if all(c == '.' for c in file_block):
+            continue
+        file_len = len(file_block)
+
+        for i in range(0, file_index):
+            if '.' in blocks[i] and len(blocks[i]) >= file_len:
+                free_len = len(blocks[i])
+                blocks[i] = file_block
+                blocks[file_index] = ['.'] * file_len
+
+                remainder = free_len - file_len
+                if remainder > 0:
+                    blocks.insert(i+1, ['.'] * remainder)
+                break
+            
+    expanded_blocks = []
+    for b in blocks:
+        expanded_blocks.extend(b)
+    return expanded_blocks
+
+def checksum(blocks):
+    sum = 0
+
+    for i, b in enumerate(blocks):
+        if b == '.':
             continue
         else:
-            sum += i * int(dm[i])
+            sum += i * int(b)
 
     return sum
 
 with open('2024/09/input.txt') as fp:
     for line in fp:
-        e = expand('2333133121414131402') # checksum = 2858
-        # e = expand('233313312141413140211') 
-        # e = expand(line.replace('\n', ''))
-        print(e)
-        print('\n--------------------\n')
-        c = compress(e)
-        print(c)
-        print(str(checksum(c)))
-
-        # print(str(checksum(compress(expand(line.replace('\n', ''))))))
+        print(str(checksum(compress(expand(line.replace('\n', ''))))))

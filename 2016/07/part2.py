704dfc597b959451b34819931fab85ca91e6b4a2
Tim Burr <tim.r.burr@gmail.com>
2024-12-14 10:37:28 -0700

    2016, day 7 (120*)

--- 2016/07/part2.py
+++ 2016/07/part2.py
@@ -10,4 +10,5 @@ def check(s):
 
 lines = [re.split(r'\[([^\]]+)\]', line) for line in open('2016/07/input.txt')]
 parts = [(' '.join(p[::2]), ' '.join(p[1::2])) for p in lines]
-print(sum(check(sn) and not(check(hn)) for sn, hn in parts))
+# print(sum(check(sn) and not(check(hn)) for sn, hn in parts))
+print(sum(any(a == c and a != b and b+a+b in hn for a, b, c in zip(sn, sn[1:], sn[2:])) for sn, hn in parts))

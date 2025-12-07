b9a86023ebf299156238bfbd47965bf5d0850e71
Tim <tim.r.burr@gmail.com>
2025-12-06 20:01:44 -0700

    9*

--- 2025/05/part1.py
+++ 2025/05/part1.py
@@ -1,24 +1,38 @@
 
 ranges = []
 def add_range(input):
-    return
+    global ranges
+
+    substrings = input.split('-')
+    ranges.append((int(substrings[0]), int(substrings[1])))
+
+def print_ranges():
+    global ranges
+
+    for r in ranges:
+        print(str(r[0]) + "-" + str(r[1]))
 
 def is_fresh(input):
     global ranges
+
+    for r in ranges:
+        if input >= r[0] and input <= r[1]:
+            return True
 
     return False
 
 count = 0
 
-with open('2025/05/test.txt') as file:
+with open('2025/05/input.txt') as file:
     for line in file:
         line = line.replace("\n", "")
         # print(line)
         if line.count('-') > 0:
             add_range(line)
         elif len(line) == 0:
+            # print_ranges()
             continue
-        elif is_fresh(line):
+        elif is_fresh(int(line)):
             count += 1
         
 print(str(count))
--- 2025/06/part1.py
+++ 2025/06/part1.py
@@ -1,6 +1,41 @@
 
-with open('2025/06/test.txt') as file:
+lines = []
+
+def add(index):
+    global lines
+
+    sum = 0
+    for l in lines:
+        if len(l[index]) == 0 or l[index] == '+':
+            continue
+        sum += int(l[index])
+    return sum
+
+def multiply(index):
+    global lines
+
+    prod = 1
+    for l in lines:
+        if len(l[index]) == 0 or l[index] == '*':
+            continue
+        prod *= int(l[index])
+    return prod
+
+
+with open('2025/06/input.txt') as file:
     for line in file:
         line = line.replace("\n", "")
-        print(line)
+        # print(line)
+        lines.append(line.split())
+
+hw_total = 0
+signs  = lines[len(lines)-1]
+
+for i in range(len(signs)):
+    if signs[i] == "+":
+        hw_total += add(i)
+    else: # "*"
+        hw_total += multiply(i)
+
+print(str(hw_total))
         
